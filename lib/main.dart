21060178347ed11781277369d8a2d75750e5f0f0
Renan Johny Ortega <renanortega82@gmail.com>
2025-02-12 16:01:17 -0300

    build: add packageas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:champions_chromo_app/presentation/pages/cart/cart_page.dart';
+import 'package:champions_chromo_app/presentation/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => CartProvider()),
+    ],
+    child: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +18,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: SplashPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
